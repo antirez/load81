34addc253df88aeebe828dc8da49bfeb9a9a058a
antirez <antirez@gmail.com>
2012-03-03 15:57:45 +0100

    text example show an usage of string.format to display current mouse coordinates into the drawing area.

--- examples/text.lua
+++ examples/text.lua
@@ -4,4 +4,6 @@ function draw()
     text(20,HEIGHT-40,"Please, enter this window with your mouse pointer.")
     fill(255,0,0,1)
     text(mouse.x,mouse.y,"Hello World!")
+    fill(200,200,200,1)
+    text(0,0,string.format("Mouse is at x:%s y:%s",mouse.x,mouse.y))
 end
