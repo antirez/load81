5f0d6867c90dcadf924ff2daa26f99d236aa3a17
antirez <antirez@gmail.com>
2012-03-03 12:38:40 +0100

    Added 'paint.lua' example showing mouse events.

---
+++ examples/paint.lua
@@ -0,0 +1,12 @@
+function setup()
+    background(0,0,0,0)
+end
+
+function draw()
+    if mouse.pressed['1'] then
+        fill(255,0,0,.2)
+    else
+        fill(0,0,255,.2)
+    end
+    ellipse(mouse.x,mouse.y,30,30)
+end
