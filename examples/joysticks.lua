f8d8c33dddf1ad10abf571f5ef23ad97e7b5027b
Jay Vaughan <jay@allaboutapps.at>
2012-03-13 18:45:09 +0100

    Slightly cleaner demo

--- examples/joysticks.lua
+++ examples/joysticks.lua
@@ -1,18 +1,22 @@
+--
+-- simple demo of joystick input
+--
+
 function setup()
     background(0,0,0,0)
+    cell_size = HEIGHT / 4;
+end
+
+function draw_joystick_info(joynum)
+    text(10, joynum * cell_size, string.format("# %d %s x:%d/y:%d",
+        joynum, joystick[joynum].name,
+        joystick[joynum].x, joystick[joynum].y));
 end
 
 function draw()
-
- background(0,0,0);
- 
- fill(0,255,0,1)
- text(10,HEIGHT/2,string.format("#JOYSTICKS=%d %s x:%d/y:%d %s x:%d/y:%d %s x:%d/y:%d %s x:%d/y:%d",
-    JOYSTICKS, 
-    joystick[1].name, joystick[1].x, joystick[1].y,
-    joystick[2].name, joystick[2].x, joystick[2].y,
-    joystick[3].name, joystick[3].x, joystick[3].y,
-    joystick[4].name, joystick[4].x, joystick[4].y
-    ))
-
+    fill (0,255,0,1);
+    background(0,0,0,0);
+    for jn = 1, JOYSTICKS, 1 do
+        draw_joystick_info(jn);
+    end 
 end
