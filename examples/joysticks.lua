b6990e5b056e5a9ddd71bb53620425bb945a892c
Jay Vaughan <jay@allaboutapps.at>
2012-03-17 23:47:24 +0100

    Slight tweaks to the joystick demo.

--- examples/joysticks.lua
+++ examples/joysticks.lua
@@ -1,17 +1,7 @@
 --
 -- simple demo of joystick input
--- by torpor (seclorum@me.com)
---
--- the joystick[] table is global, and contains .x/.y/.name fields
--- describing the joystick.  the JOYSTICK global contains the
--- number of physical joysticks detected by LOAD81 at startup.
---
--- the maximum number of joysticks available is 4.
 --
 
--- each joystick will have its own color, and since there 
--- a maximum of 4 joysticks available then we have 4
--- unique colors to use
 colors = {}
 colors = {
 {r=255,g=0,b=255,a=1},
@@ -21,47 +11,36 @@ colors = {
 }
 
 function setup()
-    print("Number of joysticks", joystick.count);
     background(0,0,0,0)
-    -- divide the screen in 4, one quad for each joystick device
-    joy_quad = HEIGHT / 4;
+    cell_size = HEIGHT / 4;
 end
 
--- simple map function
 function map(x, in_min, in_max, out_min, out_max)
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 end
 
 function draw_joystick_info(joynum)
-    -- each joystick gets its own color
     fill(colors[joynum].r,colors[joynum].g,colors[joynum].b,colors[joynum].a);
-    
-    -- text explaining joystick name and x/y values goes in a 'quarter' of the screen
-    text(10, joynum * joy_quad, string.format("# %d %s x:%d/y:%d button:%d",
+    text(10, joynum * cell_size, string.format("# %d %s x:%d/y:%d button:%d",
         joynum, joystick[joynum].name,
-        joystick[joynum].x, joystick[joynum].y, joystick[joynum].button));
+        joystick[joynum].x, joystick[joynum].y,
+        joystick[joynum].button));
 
-    -- each joystick returns x/y axes values from -32767 to 32767, so we map to 
-    -- the screen size
     dot_x, dot_y = 0;
-    dot_x = map(joystick[joynum].x, 32767, -32767, WIDTH, 0);
+    dot_x = map(joystick[joynum].x, 32767, -32767, HEIGHT, 0) + WIDTH / 4;
     dot_y = map(joystick[joynum].y, 32767, -32767, 0, HEIGHT);
+    ellipse(dot_x, dot_y, 30, 20);
 
-    -- draw the joystick dot
-    ellipse(dot_x, dot_y, 10, 10);
 end
 
-
 function draw()
-    -- clear the screen
+    fill (0,255,0,1);
     background(0,0,0,0);
 
     if joystick.count == 0 then
-        fill (255,0,0,1);
-        text(10, joy_quad, string.format("No joysticks detected .. plug one in and try again!"));
+        text(10, cell_size, string.format("No joysticks detected .. plug one in and try again!"));
     end
 
-    -- draw the info for each joystick
     for jn = 1, joystick.count, 1 do
         draw_joystick_info(jn);
     end 
